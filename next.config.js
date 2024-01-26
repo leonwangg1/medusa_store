adfdebcbdbb93dd4961b5a2130823a22fb1da10a
Leon <itsleonwang1@gmail.com>
2024-01-26 14:11:31 +1100

    Add additional domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -26,7 +26,10 @@ const nextConfig = withStoreConfig({
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
     ],
-    domains: ["https://mimiblooms-backend-c8a52735e2f5.herokuapp.com"],
+    domains: [
+      "https://mimiblooms-backend-c8a52735e2f5.herokuapp.com",
+      "https://res.cloudinary.com/ddedfwsge",
+    ],
   },
 })
 
