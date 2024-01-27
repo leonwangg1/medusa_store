61b6296836951bc84666ecaf8ed6e2506d8ab8ce
Leon <itsleonwang1@gmail.com>
2024-01-27 20:01:03 +1100

    Refactor product rail and thumbnail styles

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -19,25 +19,29 @@ export default function ProductRail({
   }
 
   return (
-    <div className="content-container py-9 small:py-12">
-      <div className="flex justify-between mb-8">
-        <Text className="txt-xlarge">{collection.title}</Text>
-        <InteractiveLink href={`/collections/${collection.handle}`}>
-          <Button variant="secondary">View all</Button>
-        </InteractiveLink>
+    <>
+      <div className="content-container pt-9 small:pt-12">
+        <div className="flex justify-between mb-8">
+          <Text className="txt-xlarge">{collection.title}</Text>
+          <InteractiveLink href={`/collections/${collection.handle}`}>
+            <Button variant="secondary">View all</Button>
+          </InteractiveLink>
+        </div>
       </div>
-      <ul className="flex overflow-auto gap-x-[0.6rem] small:gap-x-4">
-        {products &&
-          products.map((product) => (
-            <li key={product.id} className="flex-none w-40 small:w-72">
-              <ProductPreview
-                productPreview={product}
-                region={region}
-                isFeatured
-              />
-            </li>
-          ))}
-      </ul>
-    </div>
+      <div className="product-container">
+        <ul className="flex overflow-auto gap-x-[0.55rem] small:gap-x-4">
+          {products &&
+            products.map((product) => (
+              <li key={product.id} className="flex-none w-40 small:w-72">
+                <ProductPreview
+                  productPreview={product}
+                  region={region}
+                  isFeatured
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
   )
 }
--- src/modules/products/components/thumbnail/index.tsx
+++ src/modules/products/components/thumbnail/index.tsx
@@ -25,7 +25,7 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
   return (
     <Container
       className={clx(
-        "relative w-full overflow-hidden p-4 bg-ui-bg-subtle shadow-elevation-card-rest rounded-large group-hover:shadow-elevation-card-hover transition-shadow ease-in-out duration-150",
+        "relative w-full overflow-hidden p-4 bg-ui-bg-subtle shadow-elevation-card-rest rounded-[4px] group-hover:shadow-elevation-card-hover transition-shadow ease-in-out duration-150",
         className,
         {
           "aspect-[12/14]": isFeatured,
