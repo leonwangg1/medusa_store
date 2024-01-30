05e324418959f890139e74eeaccd004f236c9ce8
Leon <itsleonwang1@gmail.com>
2024-01-31 09:04:11 +1100

    Add useState and useEffect hooks to payment-wrapper component

--- src/modules/checkout/components/payment-wrapper/index.tsx
+++ src/modules/checkout/components/payment-wrapper/index.tsx
@@ -2,7 +2,7 @@
 
 import { Cart, PaymentSession } from "@medusajs/medusa"
 import { loadStripe } from "@stripe/stripe-js"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import StripeWrapper from "./stripe-wrapper"
 import { PayPalScriptProvider } from "@paypal/react-paypal-js"
 
@@ -12,19 +12,21 @@ type WrapperProps = {
 }
 
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
-// const stripePromise = stripeKey ? loadStripe(stripeKey) : null
-const stripePromise = loadStripe(
-  stripeKey ??
-    "pk_live_51OcLBlLzhTvSMbn37nvo22WUC5YdrcAzEpHPoelJw5t7igbyVTPouzOVAIZcz1GZIXV4eHeitbya8nos7ka1Spkj00IH7me9aW"
-)
+const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+
 const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
 
 const Wrapper: React.FC<WrapperProps> = ({ cart, children }) => {
-  const paymentSession = cart.payment_session as PaymentSession
+  const [loadedCart, setLoadedCart] = useState(cart)
 
+  useEffect(() => {
+    setLoadedCart(cart)
+  }, [cart])
+
+  const paymentSession = cart.payment_session as PaymentSession
   const isStripe = paymentSession?.provider_id?.includes("stripe")
 
-  console.log("isStripe:", isStripe)
+  console.log("cart:", cart)
   console.log("paymentSession:", paymentSession)
   console.log("stripePromise:", stripePromise)
 
