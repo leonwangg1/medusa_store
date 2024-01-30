58dfa254c01f1c919f3761c39e8ae9db7dd7738c
Leon <itsleonwang1@gmail.com>
2024-01-31 00:11:36 +1100

    Remove commented out code and add Wrapper component

--- src/app/[countryCode]/(checkout)/checkout/page.tsx
+++ src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -38,11 +38,8 @@ export default async function Checkout() {
 
   return (
     <div className="grid grid-cols-1 small:grid-cols-[1fr_416px] content-container gap-x-40 py-12">
-      {/* <Wrapper cart={cart}>
-        <CheckoutForm />
-      </Wrapper>
-      <CheckoutSummary /> */}
-      hi
+      <Wrapper cart={cart}>a{/* <CheckoutForm /> */}</Wrapper>
+      <CheckoutSummary />
     </div>
   )
 }
