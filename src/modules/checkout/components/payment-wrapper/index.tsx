99d1a9ae2b86d002bc9e46750564ea3bb115659c
Leon <itsleonwang1@gmail.com>
2024-01-31 08:39:12 +1100

    Update payment-wrapper component

--- src/modules/checkout/components/payment-wrapper/index.tsx
+++ src/modules/checkout/components/payment-wrapper/index.tsx
@@ -13,13 +13,20 @@ type WrapperProps = {
 
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
 const stripePromise = stripeKey ? loadStripe(stripeKey) : null
-
+// const stripePromise = loadStripe(
+//   stripeKey ??
+//     "pk_live_51OcLBlLzhTvSMbn37nvo22WUC5YdrcAzEpHPoelJw5t7igbyVTPouzOVAIZcz1GZIXV4eHeitbya8nos7ka1Spkj00IH7me9aW"
+// )
 const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
 
 const Wrapper: React.FC<WrapperProps> = ({ cart, children }) => {
   const paymentSession = cart.payment_session as PaymentSession
 
   const isStripe = paymentSession?.provider_id?.includes("stripe")
+
+  console.log("isStripe:", isStripe)
+  console.log("paymentSession:", paymentSession)
+  console.log("stripePromise:", stripePromise)
 
   if (isStripe && paymentSession && stripePromise) {
     console.log("Rendering StripeWrapper")
