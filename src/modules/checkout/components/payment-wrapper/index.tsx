373db6fb6400442a20ddd96fc4218d46fb7fd610
Leon <itsleonwang1@gmail.com>
2024-02-16 17:42:21 +1100

    UPDATED: Added StripeContext to payment-wrapper component and fixed conditional rendering of card details in payment component.
    - Added createContext and StripeContext to payment-wrapper component.
    - Added StripeContext.Provider to wrap StripeWrapper component in payment-wrapper component.
    - Fixed conditional rendering of card details in payment component by using stripeReady variable from StripeContext.

--- src/modules/checkout/components/payment-wrapper/index.tsx
+++ src/modules/checkout/components/payment-wrapper/index.tsx
@@ -2,44 +2,47 @@
 
 import { Cart, PaymentSession } from "@medusajs/medusa"
 import { loadStripe } from "@stripe/stripe-js"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import StripeWrapper from "./stripe-wrapper"
 import { PayPalScriptProvider } from "@paypal/react-paypal-js"
-
-const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
-const stripePromise = stripeKey ? loadStripe(stripeKey) : null
-const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
+import { createContext } from "react"
 
 type WrapperProps = {
   cart: Omit<Cart, "refundable_amount" | "refunded_total">
   children: React.ReactNode
 }
 
+export const StripeContext = createContext(false)
+
+const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
+const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+
+const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
+
 const Wrapper: React.FC<WrapperProps> = ({ cart, children }) => {
-  const [providerId, setProviderId] = useState<string | null>(null)
-  const isStripe = providerId?.includes("stripe")
+  const paymentSession = cart.payment_session as PaymentSession
 
-  useEffect(() => {
-    const paymentSession = cart.payment_session as PaymentSession
+  const isStripe = paymentSession?.provider_id?.includes("stripe")
 
-    setProviderId(paymentSession?.provider_id)
-  }, [])
-
-  if (!providerId) return null
-
-  if (isStripe && providerId && stripePromise) {
+  if (isStripe && paymentSession && stripePromise) {
     return (
-      <StripeWrapper
-        paymentSession={cart.payment_session!}
-        stripeKey={stripeKey}
-        stripePromise={stripePromise}
-      >
-        {children}
-      </StripeWrapper>
+      <StripeContext.Provider value={true}>
+        <StripeWrapper
+          paymentSession={paymentSession}
+          stripeKey={stripeKey}
+          stripePromise={stripePromise}
+        >
+          {children}
+        </StripeWrapper>
+      </StripeContext.Provider>
     )
   }
 
-  if (providerId === "paypal" && paypalClientId !== undefined && cart) {
+  if (
+    paymentSession?.provider_id === "paypal" &&
+    paypalClientId !== undefined &&
+    cart
+  ) {
     return (
       <PayPalScriptProvider
         options={{
